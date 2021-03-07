4514491ec1a0d5854ff3be6185f48f8e7eb6ed8e
Kevin Kwon <kevindkwon@gmail.com>
2021-03-07 09:21:04 -0500

    add try catch error, and install serve

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -18,30 +18,39 @@ class Profile extends Component {
         this.state = {
             data: {},
             repositories: [],
-            loading: true,
+            loading: false,
+            error: '',
         }
     }
     async componentDidMount() {
-        const profile = await
-    fetch('https://api.github.com/users/kdkwon8');
-        const profileJSON = await profile.json();
-
-        if (profileJSON) {
-            const repositories = await fetch(profileJSON.repos_url);
-            const repositoriesJSON = await repositories.json();
-
+        try {
+            const profile = await
+        fetch('https://api.github.com/users/kdkwon8');
+            const profileJSON = await profile.json();
+    
+            if (profileJSON) {
+                const repositories = await fetch(profileJSON.repos_url);
+                const repositoriesJSON = await repositories.json();
+    
+                this.setState({
+                    data: profileJSON,
+                    repositories: repositoriesJSON,
+                    loading: false,
+                });
+            }
+        }
+        catch(error) {
             this.setState({
-                data: profileJSON,
-                repositories: repositoriesJSON,
                 loading: false,
-            })
+                error: error.message,
+            });
         }
     }
     render() {
-        const { data, loading, repositories } = this.state;
+        const { data, loading, repositories, error } = this.state;
 
-        if (loading) {
-            return <div>Loading...</div>;
+        if (loading || error) {
+            return <div>{loading ? 'Loading...' : error}</div>;
         }
 
         const items = [
