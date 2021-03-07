52b138ec97b9d20c08376538a5862fd55032b8bc
Kevin Kwon <kevindkwon@gmail.com>
2021-03-07 08:34:15 -0500

    display the repositories from our Github profile on this Github Portfolio page

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -17,6 +17,7 @@ class Profile extends Component {
         super();
         this.state = {
             data: {},
+            repositories: [],
             loading: true,
         }
     }
@@ -26,14 +27,18 @@ class Profile extends Component {
         const profileJSON = await profile.json();
 
         if (profileJSON) {
+            const repositories = await fetch(profileJSON.repos_url);
+            const repositoriesJSON = await repositories.json();
+
             this.setState({
                 data: profileJSON,
+                repositories: repositoriesJSON,
                 loading: false,
             })
         }
     }
     render() {
-        const { data, loading } = this.state;
+        const { data, loading, repositories } = this.state;
 
         if (loading) {
             return <div>Loading...</div>;
@@ -47,16 +52,19 @@ class Profile extends Component {
             {label: 'location', value: data.location },
             {label: 'email', value: data.email },
             {label: 'bio', value: data.email }
-        ]
+        ];
+
+        const projects = repositories.map(repository => ({
+            label: repository.name,
+            value: <Link url={repository.html_url} title='Github URL' />
+        }));
 
         return (
             <ProfileWrapper>
                 <Avatar src={data.avatar_url} alt='avatar' />
-                <List items={items} />
+                <List title='Profile' items={items} />
+                <List title='Projects' items={projects} />
             </ProfileWrapper>
-
-
-
         );
     }
 }
